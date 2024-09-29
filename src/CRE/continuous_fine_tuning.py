9298142597750d86c68fc6b2eef726a579036890
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 <sefikaefeoglu@gmail.com>
2024-09-29 14:10:45 +0200

    adapt_for CRE

--- src/CRE/continuous_fine_tuning.py
+++ src/CRE/continuous_fine_tuning.py
@@ -30,11 +30,10 @@ max_source_length = None
 max_target_length = None
 
 def set_dataset(dataset_id):
-    # Load dataset from the hub
-    ### TODO ###
-    ## change this part hugging face dataset conversion rather than reading from HF.
+    # Load dataset
 
     dataset = load_dataset(dataset_id)
+    # print(dataset['train'][0])
     return dataset
 
 
@@ -70,8 +69,8 @@ def load_model(model_id="google/flan-t5-large"):
     
     compute_dtype = getattr(torch, "float16")
     # load model from the hub
-    maxmem={i:f'{int(torch.cuda.mem_get_info()[0]/1024**3)-2}GB' for i in range()}
-    maxmem['cpu']='300GB'
+    # maxmem={i:f'{int(torch.cuda.mem_get_info()[0]/1024**3)-2}GB' for i in range()}
+    # maxmem['cpu']='300GB'
     bnb_config=BitsAndBytesConfig(
             load_in_4bit=True,
             bnb_4bit_quant_type="nf4",
@@ -79,7 +78,8 @@ def load_model(model_id="google/flan-t5-large"):
             bnb_4bit_use_double_quant=False,
     )
 
-    model = AutoModelForSeq2SeqLM.from_pretrained(model_id,  device_map="auto", max_memory=maxmem, quantization_config=bnb_config)
+    # model = AutoModelForSeq2SeqLM.from_pretrained(model_id,  device_map="auto", max_memory=maxmem, quantization_config=bnb_config)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_id,  device_map="auto", quantization_config=bnb_config)
     return model
 
 
@@ -96,7 +96,7 @@ lora_config = LoraConfig(
             lora_dropout=0.01,
             target_modules=["k","q","v","o"]
             )
-model = load_model(model_id="google/flan-t5-large")
+model = load_model(model_id="google/flan-t5-base")
 
 metric = evaluate.load("rouge")
 
@@ -136,7 +136,7 @@ def compute_metrics(eval_preds):
 
     # model_predictions.extend(decoded_preds)
     grounds, preds = postprocess_text(decoded_labels,decoded_preds)
-    p, r, f, s = precision_recall_fscore_support(grounds, preds, labels=targets_labels, average='micro')
+    p, r, f, _ = precision_recall_fscore_support(grounds, preds, labels=targets_labels, average='micro')
     
     decoded_preds = ["\n".join(pred.strip()) for pred in decoded_preds]
 
@@ -155,33 +155,35 @@ def compute_metrics(eval_preds):
     
     return {k: round(v, 4) for k, v in result.items()}
 
-def main(dataset_id, model_id, data_path):
+def main(model_id, data_path, tasks_path, task_id):
 
-    targets = json.load(open(data_path))
-    targets_labels = [line['relation'] for line in targets]
-
-
-
-    dataset = set_dataset(dataset_id)
+    targets_labels = json.load(open(tasks_path))
+    print(targets_labels)
 
 
-    tokenizer = set_tokenizer(model_id="google/flan-t5-large")
+
+    dataset = set_dataset(data_path)
+
+    
+    tokenizer = set_tokenizer(model_id="google/flan-t5-base")
+    
     # The maximum total input sequence length after tokenization.
     # Sequences longer than this will be truncated, sequences shorter will be padded.
-    tokenized_inputs = concatenate_datasets([dataset["train"], dataset["test"]]).map(lambda x: tokenizer(x["prompt"], truncation=True), batched=True, remove_columns=["prompt", "relation"])
+    tokenized_inputs = concatenate_datasets([dataset["train"], dataset["validation"]]).map(lambda x: tokenizer(x["prompt"], truncation=True), batched=True, remove_columns=["prompt", "relation"])
     max_source_length = max([len(x) for x in tokenized_inputs["input_ids"]])
     print(f"Max source length: {max_source_length}")
-
+    
     # The maximum total sequence length for target text after tokenization.
     # Sequences longer than this will be truncated, sequences shorter will be padded."
-    tokenized_targets = concatenate_datasets([dataset["train"], dataset["test"]]).map(lambda x: tokenizer(x["relation"], truncation=True), batched=True, remove_columns=["prompt", "relation"])
+    tokenized_targets = concatenate_datasets([dataset["train"], dataset["validation"]]).map(lambda x: tokenizer(x["relation"], truncation=True), batched=True, remove_columns=["prompt", "relation"])
     max_target_length = max([len(x) for x in tokenized_targets["input_ids"]])
     print(f"Max target length: {max_target_length}")
     tokenized_dataset = dataset.map(preprocess_function, batched=True, remove_columns=["prompt", "relation"])
     print(f"Keys of tokenized dataset: {list(tokenized_dataset['train'].features)}")
+    
     model = load_model(model_id)
 
-
+  
 
 
     # we want to ignore tokenizer pad token in the loss
@@ -196,7 +198,7 @@ def main(dataset_id, model_id, data_path):
 
 
     # Hugging Face repository id
-    repository_id = f"{model_id.split('/')[1]}-{dataset_id}"
+    repository_id = f"{model_id.split('/')[1]}-{task_id}"
 
     training_args=TrainingArguments(
                 output_dir=repository_id,
@@ -207,7 +209,7 @@ def main(dataset_id, model_id, data_path):
                 optim="paged_adamw_32bit",
                 save_steps=25,
                 logging_steps=25,
-                learning_rate=5e-5
+                learning_rate=2e-2,
                 fp16=False,
                 bf16=False,
                 max_grad_norm=0.3,
@@ -232,9 +234,12 @@ def main(dataset_id, model_id, data_path):
     trainer.train()
     trainer.save_model()
     trainer.model.save_pretrained(repository_id)
+    return trainer
 
 if __name__ == "__main__":
 
-    dataset_id = 'Sefika/semeval_prompts'
+    dataset_path = '/Users/sefika/phd_projects/CRE_PTM/data/tacred/continuous_prompt_fine_tuning/train/task1/'
+    tasks_path = "/Users/sefika/phd_projects/CRE_PTM/data/tacred/train_tasks/relations/task1.json"
     model_id="google/flan-t5-large"
-    main(dataset_id, model_id)
+    task_id = "task1"
+    trainer_model = main(model_id, dataset_path, tasks_path, task_id)
