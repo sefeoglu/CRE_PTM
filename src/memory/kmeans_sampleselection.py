23cdb8c54739cfc6ebc2aa77bd28e8c225df5ba8
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 <sefikaefeoglu@gmail.com>
2024-10-06 14:16:42 +0200

    add some features.

--- src/memory/kmeans_sampleselection.py
+++ src/memory/kmeans_sampleselection.py
@@ -6,7 +6,7 @@ def sample_selection_kmeans(data, memory_size):
     # Extract the embeddings and convert them into a 2D numpy array
     embeddings_array = [item['embedding'] for item in data]
     prompt_array = [item['prompt'] for item in data]
-
+    relation_array = [item['relation'] for item in data]
     # Get the minimum embedding dimension across all embeddings
     min_dim = min(emb.shape[1] for emb in embeddings_array)  
     
@@ -22,14 +22,19 @@ def sample_selection_kmeans(data, memory_size):
 
     mem_set = [] # Initialize mem_set as a list
     prompt_set = []
+    relation_set = []
+    selected_samples = []
     for i in range(num_clusters):
         sel_index = np.argmin(distances[:,i])
         instance = embeddings_array[sel_index]
         mem_set.append(instance)
         prompt = prompt_array[sel_index]
         prompt_set.append(prompt)
+        relation_set.append(relation_array[sel_index])
+        selected_samples.append({"prompt":prompt,"relation":relation_array[sel_index]})
 
     mem_set = np.array(mem_set) 
     prompt_set = np.array(prompt_set)
-    
-    return mem_set, prompt_set
+    relation_set = np.array(relation_set)
+
+    return mem_set, prompt_set, relation_set, selected_samples
