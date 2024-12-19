6c74fc679253964c0d6c50b86df7f1b69d4b4d51
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 <sefikaefeoglu@gmail.com>
2024-12-19 19:15:05 +0100

    update codes

--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,11 @@
 import sys 
 import os
+import json
+def read_json(path):
+    with open(path, 'r', encoding="utf-8") as f:
+        data = json.load(f)
+    return data
+
+def write_json(data, path):
+    with open(path, 'w', encoding="utf-8") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
