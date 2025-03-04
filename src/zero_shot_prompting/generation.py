d0b0c4e51bc246edc5adedc3c1d06072e8405671
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 <sefikaefeoglu@gmail.com>
2025-03-04 23:07:12 +0100

    remove hardcode

--- src/zero_shot_prompting/generation.py
+++ src/zero_shot_prompting/generation.py
@@ -1,12 +1,11 @@
 """Generates responses for prompts using a language model defined in Hugging Face."""
 """Created by: Sefika"""
 
-import json
 import torch
-from transformers import AutoTokenizer
-from transformers import AutoModelForCausalLM
 
 from transformers import T5Tokenizer, T5ForConditionalGeneration
+from utils import read_json, write_json
+import configparser
 
 class LLM(object):
 
@@ -16,12 +15,9 @@ class LLM(object):
         Args:
             model_id (str, optional): model name from Hugging Face. Defaults to "google/flan-t5-base".
         """
-        self.maxmem={i:f'{int(torch.cuda.mem_get_info()[0]/1024**3)-2}GB' for i in range(4)}
-        self.maxmem['cpu']='300GB'
-        if model_id=="google/flan-t5-base":
-            self.model, self.tokenizer = self.get_model(model_id)
-        else:
-            self.model, self.tokenizer = self.get_model_decoder(model_id)
+
+
+        self.model, self.tokenizer = self.get_model(model_id)
 
 
     def get_model(self, model_id="google/flan-t5-base"):
@@ -63,24 +59,12 @@ class LLM(object):
 
         return response
 
-def read_json(path):
-    with open(path, 'r', encoding="utf-8") as f:
-        data = json.load(f)
-    return data
-
-def write_json(data, path):
-    with open(path, 'w', encoding="utf-8") as f:
-        json.dump(data, f, indent=4, ensure_ascii=False)
-
 def main(input_path, output_path):
   llm_instance = LLM()
-  tokenizer = llm_instance.tokenizer
-  model = llm_instance.model
   data = read_json(input_path)
-  responses = []
-  relations = []
+  responses, relations = [], []
+
   for i, item in enumerate(data):
-    # print(item)
     prompt = item['prompt']
     relations.append(item['relation'])
 
@@ -94,12 +78,19 @@ def main(input_path, output_path):
       responses.append({"predict":response[0], "ground_truth": item['relation']})
 
   write_json(responses, output_path)
+  
 if __name__ =="__main__":
-  ## TODO ##
-  ## : Change the paths ##
-  for j in range(1, 6):
-    for i in range(1, 11):
-        output_path = f"drive/MyDrive/ESWC-figures/fewrel/zero-shot/result_task_{j}_{i}.json"
-        input_path = f"t5_format/test/run_{j}/task{i}/test_1.json"
+   configparser_path = "config.ini"
+   config = configparser.ConfigParser()
+   config.read(configparser_path)
+   input_path = config['DEFAULT']['input_path']
+   output_path = config['DEFAULT']['output_path']
+   number_of_tasks = int(config['DEFAULT']['number_of_tasks'])
+   run_number = int(config['DEFAULT']['run_number'])
+
+   for j in range(1, run_number+1):
+      for i in range(1, number_of_tasks+1):
+        output_path = f"{output_path}/result_task_{j}_{i}.json"
+        input_path = f"{input_path}/run_{j}/task{i}/test_1.json"
         main(input_path, output_path)
 
