a2cf1d12ddad570a4b577256710ff73ce76f8fd9
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 <sefikaefeoglu@gmail.com>
2025-05-30 12:45:51 +0200

    update codes and remove unused

--- src/clean/clean_decoder_results.py
+++ src/clean/clean_decoder_results.py
@@ -24,43 +24,26 @@ def write_json(data, path):
         json.dump(data, f, indent=4, ensure_ascii=False)
 # Sample data
 
-def extract_answers(data):
-    results = []
-    for entry in data:
 
-        predict_text = entry["predict"]
-        # Extract the sentence
-        # print(predict_text)
-
-        # Extract the relation type
-        relation = predict_text.split("### Answer:")[1].strip().split('\n')[0].split(',')[0].split('.')[0].split(' ')[0]
-        answer_match = re.search(r"Answer: (.*?)\n", predict_text)
-
-
-        results.append({
-                "sentence": predict_text,
-                "relation": relation.replace('#','')
-            })
-        # break
-    print(results)
-    return results
 def extact_answers(data, relation_types):
     for relation in relation_types:
         if relation in data:
             return relation
     return ''
 
-def clean(data, task_relations_path):
+def clean(data, task_relations_path, model_name='llama'):
     task_relations = read_json(task_relations_path)
     relation_types = [item['relation'].replace(' ', "_") for item in task_relations]
     relation_types = list(set(relation_types))
-    delimiter = "[/INST] ### Answer:"
+    
+    delimiter = "Answer:"
     answers = []
     for i, item in enumerate(data):
         item['predict'] = item['predict'].replace("Answer: \n",delimiter)
         item['predict'] = item['predict'].replace("Answer:\n",'Answer:')
         answer = item['predict'].split(delimiter)[1]
-        answer = answer.split("\n")[0].replace("#","").rstrip()
+        if model_name != 'llama':
+            answer = answer.split("\n")[0].replace("#","").rstrip()
         answer = ' '.join(answer.split())
         answer = extact_answers(answer, relation_types)
         line = {"id":i, "predict":answer, "original":item['predict']}
@@ -69,19 +52,16 @@ def clean(data, task_relations_path):
     return answers
 
 def get_answers(folder_path, task_relations_path):
-    # Print results
+ 
     try:
         for i in range(1, 6):
-            # llama_results/m_10/KMmeans_CRE_tacred
+
             folder = f'{folder_path}_{i}'
             for file in os.listdir(folder):
                 file_path = os.path.join(folder, file)
                 print(file)
                 if file != ".ipynb_checkpoints" :
                     data =  read_json(file_path)
-
-                    # # Extract answers
-                    # extracted_answers = extract_answers(data)
                     extracted_answers = clean(data, task_relations_path)
                     out_path = os.path.join(folder+'_extracted', file)
                     os.makedirs(os.path.dirname(out_path), exist_ok=True)
@@ -96,4 +76,4 @@ if __name__ == "__main__":
     config.read(PREFIX_PATH + 'config.ini')
     results_path = config['TEST']['results_path']
     tasks_path = config['TEST']['tasks_path']
-    get_answers(results_path, tasks_path)
+    get_answers(results_path, tasks_path, model_name='llama')
